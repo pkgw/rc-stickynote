6f913e7092ce9b94a6eabea77367eb8d7a198287
Peter Williams <peter@newton.cx>
2020-02-23 14:36:54 -0500

    semi-pi-gen: work on customizations for the rc-stickynote application

---
+++ semi-pi-gen/stage2/90-rc-stickynote/00-run.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+#
+# Custom setup for the rc-stickynote application.
+
+set -xeuo pipefail
+
+local_dir="$(cd $BASE_DIR/../local && pwd)"
+pi_user_home="$ROOTFS_DIR/home/$FIRST_USER_NAME"
+rust_bin_dir="$(cd $BASE_DIR/../target/armv7-unknown-linux-gnueabihf/release && pwd)"
+
+# The key binary:
+
+mkdir -p "$ROOTFS_DIR/usr/local/bin"
+cp $rust_bin_dir/rc_stickynote_displayer "$ROOTFS_DIR/usr/local/bin/"
+
+# SSH key for connecting to the Hub server:
+
+mkdir -p "$pi_user_home/.ssh"
+cp $local_dir/stickynote_*_key* "$pi_user_home"/.ssh
+chmod 600 "$pi_user_home"/.ssh/stickynote_*
+
+on_chroot <<EOF
+chown -R $FIRST_USER_NAME:$FIRST_USER_NAME /home/$FIRST_USER_NAME/.ssh
+EOF
+
+# config file for the client:
+
+mkdir -p "$pi_user_home/.config/rc-stickynote-client"
+cp $local_dir/client-config.toml "$pi_user_home/.config/rc-stickynote-client/rc-stickynote-client.toml"
+
+on_chroot <<EOF
+chown -R $FIRST_USER_NAME:$FIRST_USER_NAME /home/$FIRST_USER_NAME/.config
+EOF
+
+# init script to display IP addresses upon bootup and clear the display upon
+# poweroff, should all else fail:
+
+mkdir -p "$ROOTFS_DIR/etc/init.d"
+cp assets/rc-stickynote-display-ip "$ROOTFS_DIR/etc/init.d"
+
+on_chroot <<EOF
+update-rc.d rc-stickynote-display-ip defaults
+EOF

0ee95a461b2fe1c57e8e513a4dd8accdc88b2e6f
Peter Williams <peter@newton.cx>
2020-02-24 13:18:35 -0500

    semi-pi-gen: rework the housekeeping service

--- semi-pi-gen/stage2/90-rc-stickynote/00-run.sh
+++ semi-pi-gen/stage2/90-rc-stickynote/00-run.sh
@@ -32,14 +32,14 @@ on_chroot <<EOF
 chown -R $FIRST_USER_NAME:$FIRST_USER_NAME /home/$FIRST_USER_NAME/.config
 EOF
 
-# init scripts. The `display-ip` one displays IP addresses upon bootup and
-# clears the display upon poweroff, should all else fail:
+# init scripts. The `housekeeping` one clears the display upon poweroff, and can
+# show IP addresses with its `status` command.
 
 mkdir -p "$ROOTFS_DIR/etc/init.d"
-cp assets/rc-stickynote-display-ip "$ROOTFS_DIR/etc/init.d"
+cp assets/rc-stickynote-housekeeping "$ROOTFS_DIR/etc/init.d"
 cp assets/rc-stickynote-displayer "$ROOTFS_DIR/etc/init.d"
 
 on_chroot <<EOF
-update-rc.d rc-stickynote-display-ip defaults
+update-rc.d rc-stickynote-housekeeping defaults
 update-rc.d rc-stickynote-displayer defaults
 EOF

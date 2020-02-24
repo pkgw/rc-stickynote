d7ec2673f5cff943c3c81a12e7ad430d61ba7fca
Peter Williams <peter@newton.cx>
2020-02-24 15:06:38 -0500

    displayer/src/epd7in5.rs: work around unpriv GPIO timing bug
    See https://github.com/rust-embedded/rust-sysfs-gpio/issues/5 .

--- displayer/src/epd7in5.rs
+++ displayer/src/epd7in5.rs
@@ -11,7 +11,7 @@ use linux_embedded_hal::{
     sysfs_gpio::Direction,
     Delay, Pin, Spidev,
 };
-use std::io::Error;
+use std::{io::Error, thread::sleep, time::Duration};
 
 use super::DisplayBackend;
 
@@ -43,23 +43,33 @@ impl DisplayBackend for EPD7in5Backend {
         let cs = Pin::new(8); // Chip Select pin
         cs.export().expect("cs export");
         while !cs.is_exported() {}
+        // See https://github.com/rust-embedded/rust-sysfs-gpio/issues/5 --
+        // after the pin is exported, there is a small window before the
+        // RPi udev system changes permissions on the created device file.
+        // If we try to set the direction before this window elapses, we
+        // fail with EACCES when run as non-root. We're only booting up
+        // infrequently, so just hardcode a delay.
+        sleep(Duration::from_millis(750));
         cs.set_direction(Direction::Out).expect("CS Direction");
         cs.set_value(1).expect("CS Value set to 1");
 
         let busy = Pin::new(24); // Busy pin
         busy.export().expect("busy export");
         while !busy.is_exported() {}
+        sleep(Duration::from_millis(750)); // see above
         busy.set_direction(Direction::In).expect("busy Direction");
 
         let dc = Pin::new(25);
         dc.export().expect("dc export");
         while !dc.is_exported() {}
+        sleep(Duration::from_millis(750)); // see above
         dc.set_direction(Direction::Out).expect("dc Direction");
         dc.set_value(1).expect("dc Value set to 1");
 
         let rst = Pin::new(17);
         rst.export().expect("rst export");
         while !rst.is_exported() {}
+        sleep(Duration::from_millis(750)); // see above
         rst.set_direction(Direction::Out).expect("rst Direction");
         rst.set_value(1).expect("rst Value set to 1");
 
